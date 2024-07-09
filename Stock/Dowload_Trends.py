c3f734d760059c34eae504f2e4bb9d741b064d16
u1973238 <u1973238@campus.udg.edu>
2024-07-09 10:45:28 +0200

    Reordenar, dowload data i LSTM amb multi trends

--- Stock/Dowload_Trends.py
+++ Stock/Dowload_Trends.py
@@ -3,8 +3,9 @@ import time
 import pandas as pd
 import yfinance as yf
 
+# Downloading Daily Historical Stock Data
 stock_symbol = 'AAPL'
-start_date = '2024-05-01'
+start_date = '2023-07-01'
 end_date = '2024-07-01'
 
 # Function to fetch Google Trends data with retry mechanism
@@ -33,8 +34,8 @@ trends_data_list = []
 for keyword in keywords:
     trends_data = fetch_google_trends_data(keyword, start_date, end_date)
     pd_trends_data = pd.DataFrame(trends_data)
-    pd_trends_data = pd_trends_data.resample('D').interpolate()
-    
+    pd_trends_data = pd_trends_data.resample('B').interpolate()
+
     # Min-Max normalization of the trend data
     min_val = pd_trends_data[keyword].min()
     max_val = pd_trends_data[keyword].max()
@@ -45,11 +46,10 @@ for keyword in keywords:
 # Combining the Google Trends data
 combined_trends_data = pd.concat(trends_data_list, axis=1)
 
-# Downloading Daily Historical Stock Data
 stock_data = yf.download(stock_symbol, start=start_date, end=end_date)
 
 # Creating a date range for the entire period
-full_date_range = pd.date_range(start=start_date, end=end_date, freq='D')  # 'B' frequency is for business days
+full_date_range = pd.date_range(start=start_date, end=end_date, freq='B')  # 'B' frequency is for business days
 
 # Reindexing the stock data to include all dates in the full date range and using forward fill
 stock_data = stock_data.reindex(full_date_range).ffill()
@@ -59,11 +59,12 @@ merged_data = stock_data.join(combined_trends_data, how='inner')
 
 # Creating the target column
 merged_data['target'] = merged_data['Close'].shift(-1) - merged_data['Close']
-
+'''
 # Min-Max normalization of the target column
 min_target = merged_data['target'].min()
 max_target = merged_data['target'].max()
 merged_data['normalized_target'] = 2 * (merged_data['target'] - min_target) / (max_target - min_target) - 1
-
+'''
 # Saving the DataFrame to a CSV file
 merged_data.to_csv('merged_data_with_normalized_target.csv', index=True)
+
